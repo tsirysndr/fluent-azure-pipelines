a796987c107896685caf88b8f9e4d414f5b1711a
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-29 18:36:28 +0000

    add explicit versions

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { assertEquals } from "jsr:@std/testing/asserts";
+export { assertEquals } from "jsr:@std/testing@0.217.0/asserts";
 export { stringify } from "npm:yaml@2.3.1";
 export { z } from "npm:zod@3.22.1";
